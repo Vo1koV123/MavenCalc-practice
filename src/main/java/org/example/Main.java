394cbc39c880bf52ba6d8ef51ae2304d538152a9
bodya <bodyanka229@gmail.com>
2023-05-09 18:19:40 +0300

    completed the task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Invalid number of arguments. Please provide two values and an operation (+, -, /, x)");
+            return;
+        }
 
+        double value1 = Double.parseDouble(args[0]);
+        double value2 = Double.parseDouble(args[1]);
+        String operation = args[2];
+
+        double result = 0;
+        switch (operation) {
+            case "+": result = value1 + value2; break;
+            case "-": result = value1 - value2; break;
+            case "/": result = value1 / value2; break;
+            case "x": result = value1 * value2; break;
+            default: {
+                System.out.println("Invalid operation. Please use one of the following: +, -, /, x");
+                return;
+            }
+        }
+
+        System.out.println("Result: " + result);
     }
 }
